2c5073a7f3d788227eb089f2620a114f523fa659
h-ueno2 <menbou1212@gmail.com>
2020-07-18 15:33:28 +0900

    signinとsignupの画面作成

---
+++ src/views/Signin.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-form v-model="valid">
+    <v-container fill-hegiht>
+      <v-row>
+        <v-text-field
+          v-model="username"
+          lavel="e-mailアドレスを入力してください"
+          required></v-text-field>
+      </v-row>
+      <v-row>
+        <v-text-field
+          v-model="password"
+          lavel="passwordを入力してください"
+          required></v-text-field>
+      </v-row>
+      <v-row>
+        <v-btn>登録</v-btn>
+      </v-row>
+      <v-row>
+        アカウントを新規作成しますか？
+      <router-link to="/signup">sign up now!!</router-link>
+      </v-row>
+    </v-container>
+  </v-form>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Signin extends Vue {
+  public username: string = '';
+  public password: string = '';
+  public valid: boolean = false;
+}
+</script>
---
+++ src/views/Signup.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-form v-model="valid">
+    <v-container fill-hegiht>
+      <v-row>
+        <v-text-field
+          v-model="username"
+          lavel="e-mailアドレスを入力してください"
+          required></v-text-field>
+      </v-row>
+      <v-row>
+        <v-text-field
+          v-model="password"
+          lavel="passwordを入力してください"
+          required></v-text-field>
+      </v-row>
+      <v-row>
+        <v-btn>登録</v-btn>
+      </v-row>
+      <v-row>
+        アカウントを持っていますか？
+      <!-- <router-link to="/signin">sign in now!!</router-link> -->
+      </v-row>
+    </v-container>
+  </v-form>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Signup extends Vue {
+  public username: string = '';
+  public password: string = '';
+  public valid: boolean = false;
+}
+</script>
