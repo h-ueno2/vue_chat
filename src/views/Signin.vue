b3b019c976314b3d696da832431cc149d89def12
h-ueno2 <menbou1212@gmail.com>
2020-07-19 11:38:57 +0900

    Signin画面のレイアウト調整

--- src/views/Signin.vue
+++ src/views/Signin.vue
@@ -1,38 +1,42 @@
 <template>
-  <v-form v-model="valid">
-    <v-container fill-hegiht>
-      <v-row>
-        <v-text-field
-          v-model="username"
-          lavel="e-mailアドレスを入力してください"
-          required></v-text-field>
-      </v-row>
-      <v-row>
-        <v-text-field
-          v-model="password"
-          lavel="passwordを入力してください"
-          required></v-text-field>
-      </v-row>
-      <v-row>
-        <v-btn @click="signin">ログイン</v-btn>
-      </v-row>
-      <v-row>
+  <v-card class="pa-10 ma-10">
+    <v-form 
+      ref="form"
+      v-model="valid"
+      lazy-validation>
+      <v-text-field
+        v-model="username"
+        label="E-mail"
+        required
+        outlined></v-text-field>
+      <v-text-field
+        v-model="password"
+        label="Password"
+        required
+        outlined></v-text-field>
+      <v-btn @click="signin">ログイン</v-btn>
+      <v-card-text>
         アカウントを新規作成しますか？
-      <router-link to="/signup">sign up now!!</router-link>
-      </v-row>
-    </v-container>
-  </v-form>
+        <router-link to="/signup">sign up now!!</router-link>
+      </v-card-text>
+    </v-form>
+  </v-card>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import firebase from 'firebase';
+import VForm from '@/modlues/VForm.vue';
 
 @Component
 export default class Signin extends Vue {
   public username: string = '';
   public password: string = '';
-  public valid: boolean = false;
+  public valid: boolean = true;
+
+  get form(): VForm {
+    return (this.$refs as any).form;
+  }
 
   public signin() {
     firebase.auth().signInWithEmailAndPassword(this.username, this.password)
