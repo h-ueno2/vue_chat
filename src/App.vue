4f648fa74767be73b5c52ed802d7d54f172c5921
h-ueno2 <menbou1212@gmail.com>
2020-07-22 20:33:09 +0900

    Signin状態によってSignoutボタンの表示有無を制御したい。App.vueにRouteの設定を取得するロジックを追加

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,7 @@
 <script lang="ts">
 import { Component, Vue, Watch } from 'vue-property-decorator';
 import NavBar from './components/templates/NavBar.vue';
+import { RouteConfig, Route } from 'vue-router';
 
 @Component({
   name: 'App',
@@ -18,6 +19,15 @@ import NavBar from './components/templates/NavBar.vue';
   },
 })
 export default class App extends Vue {
+  private isSigned: boolean = false;
 
+  @Watch('$route')
+  private onRouteChanged(route: Route) {
+    const requiresAuth = route.matched.some((record) => record.meta.requiresAuth);
+    // requiresAuthが必要なページであればログイン済みとする。
+    // Firebaseにアクセスしてログイン状態を判定する方法も考えられるが、
+    // 通信が気になるのでアプリケーション内で完結させたい。
+    this.isSigned = requiresAuth;
+  }
 }
 </script>
