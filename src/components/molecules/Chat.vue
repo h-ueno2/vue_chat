b6fe03a21b8db863b40b769a51ec1e8a7a1a4f5a
h-ueno2 <menbou1212@gmail.com>
2020-07-24 18:44:25 +0900

    チャット機能の作成。メッセージの取得をお試し

--- src/components/molecules/Chat.vue
+++ src/components/molecules/Chat.vue
@@ -39,15 +39,32 @@ export default class Chat extends Vue {
   public created() {
     firebase.auth().onAuthStateChanged((user) => {
       this.user = user;
-      // const ref_message = firebase.database().ref('message');
-
+      const refMessage = firebase.database().ref('message');
+      if (user) {
+        this.messages = [];
+        refMessage.limitToLast(10).on('child_added', this.chilidAdded);
+      } else {
+        refMessage.limitToLast(10).on('child_added', this.chilidAdded);
+      }
     });
   }
 
+  // 受け取ったメッセージを追加
+  // Firebaseのデータベースに新しい要素が追加されると随時呼び出しする
+  public chilidAdded(snap: firebase.database.DataSnapshot) {
+    const message = snap.val() as Message;
+    this.messages.push({
+      key: snap.key || '',
+      name: message.name,
+      text: message.text,
+    });
+  }
+
+  // メッセージの送信
   public send() {
     if (this.user && this.user.uid && this.input.length > 0) {
       const message: Message = {
-        name: this.user.displayName || '',
+        name: this.user.email || '',
         text: this.input,
       };
       firebase.database().ref('message').push(message, () => {
