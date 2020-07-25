e0c55540b0f87b88e40aa2fb43238dee7eefbbce
h-ueno2 <menbou1212@gmail.com>
2020-07-25 09:10:34 +0900

    メッセージの表示をコンポーネント化

---
+++ src/components/molecules/ChatDispMessage.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-container>
+    {{ message.name }}, {{ message.text }}
+  </v-container>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop, Emit } from 'vue-property-decorator';
+import { Message } from '../../modules/Message';
+
+@Component({
+  name: 'ChatDispMessage',
+})
+export default class ChatDispMessage extends Vue {
+  @Prop({required: true})
+  public message!: Message;
+}
+</script>
+
--- src/components/molecules/ChatMessageArea.vue
+++ src/components/molecules/ChatMessageArea.vue
@@ -1,8 +1,10 @@
 <template>
   <v-container>
     <transition-group>
-      <section v-for="{ key, name, text } in messages" :key="key">
-        {{ name }}, {{ text }}
+      <section v-for="message in messages" :key="message.key">
+        <ChatDispMessage
+          :message="message"
+          />
       </section>
     </transition-group>
   </v-container>
@@ -11,10 +13,14 @@
 <script lang="ts">
 import { Component, Vue, Prop, Emit } from 'vue-property-decorator';
 import { Message } from '../../modules/Message';
+import ChatDispMessage from './ChatDispMessage.vue';
 
 
 @Component({
   name: 'ChatMessageArea',
+  components: {
+    ChatDispMessage,
+  },
 })
 export default class ChatMessageArea extends Vue {
   @Prop({default: () => {}})
